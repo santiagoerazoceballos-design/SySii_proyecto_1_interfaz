0ca95f3bfd9bc8a00d61bdaf57625d4d5598415f
SebAyalaUnivalle <sebastian.nova@correounivalle.edu.co>
2026-04-10 00:54:20 -0500

    .

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+String fullVolt, Vstring1, Vstring2, espacio = " ";
+
+void setup() {
+  pinMode(15, INPUT);
+  pinMode(4, INPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  float volt1=(analogRead(15))*(3.3/1023);
+  float volt2=(analogRead(4))*(3.3/1023);
+  Vstring1 = String(volt1);
+  Vstring2 = String(volt2);
+
+  fullVolt = Vstring1 + espacio + Vstring2;
+  Serial.println(fullVolt);
+  delay(1);
+}
